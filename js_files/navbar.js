cac309ed883ea42a9f00afff70d46db6e1fac66e
CristinaBDC <clescai@yahoo.com>
2024-02-12 09:51:37 +0200

    add Project Page, Contact Page and updated index.html

---
+++ js_files/navbar.js
@@ -0,0 +1,8 @@
+//functionality for loading the content of a navigation menu (navbar) from an external HTML file and displaying it in a specific container on the page.
+fetch('../html_pages/navbar.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('navbarContainer').innerHTML = data;
+    })
+    .catch(error => console.error(error));
+    
