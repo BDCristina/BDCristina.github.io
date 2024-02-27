77542f0ad6ad406233eb827cf53219708afd8733
CristinaBDC <clescai@yahoo.com>
2024-02-27 09:33:46 +0200

    Improved navbar functionality and mobile user experience.

--- js_files/navbar.js
+++ js_files/navbar.js
@@ -6,3 +6,35 @@ fetch('../html_pages/navbar.html')
     })
     .catch(error => console.error(error));
     
+document.querySelector('.burger-icon').addEventListener('click', function() {
+    document.querySelector('.navigation').classList.toggle('menu-open');
+});
+
+window.addEventListener('resize', function() {
+    if (window.innerWidth > 768) {
+        document.querySelector('.navigation').classList.remove('menu-open');
+    }
+});
+
+function toggleMenu() {
+    var modal = document.getElementById("modalContainer");
+    modal.style.display === "block" ? modal.style.display = "none" : modal.style.display = "block";
+}
+
+window.onclick = function(event) {
+    var modal = document.getElementById("modalContainer");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const burgerIcon = document.querySelector('.burger-icon');
+    const navigation = document.querySelector('.navigation');
+
+    burgerIcon.addEventListener('click', function() {
+        // Toggle class 'menu-open' pe meniu pentru a-l afișa sau ascunde
+        navigation.classList.toggle('menu-open');
+    });
+});
+    
